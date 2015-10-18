d7459e37dcaf42c3ac4e03fe0cf88552a7c4549c
Warlander <warlander@wurmonline.com>
2015-10-18 03:33:20 +0200

    Switched lead and wall tiles colors.

--- src/com/wurmonline/wurmapi/internal/CaveColors.java
+++ src/com/wurmonline/wurmapi/internal/CaveColors.java
@@ -10,8 +10,8 @@ public class CaveColors {
     private static final Map<Tile, Color> mappings = new HashMap<>();
     
     static {
-        addMapping(Tile.TILE_CAVE_WALL, Color.PINK.darker());
-        addMapping(Tile.TILE_CAVE_WALL_REINFORCED, Color.PINK.darker().darker());
+        addMapping(Tile.TILE_CAVE_WALL, Color.DARK_GRAY);
+        addMapping(Tile.TILE_CAVE_WALL_REINFORCED, Color.DARK_GRAY);
         addMapping(Tile.TILE_CAVE, Color.PINK);
         addMapping(Tile.TILE_CAVE_FLOOR_REINFORCED, Color.PINK);
         addMapping(Tile.TILE_CAVE_EXIT, Color.PINK);
@@ -23,7 +23,7 @@ public class CaveColors {
         addMapping(Tile.TILE_CAVE_WALL_ORE_ADAMANTINE, Color.CYAN);
         addMapping(Tile.TILE_CAVE_WALL_ORE_GLIMMERSTEEL, Color.YELLOW.brighter());
         addMapping(Tile.TILE_CAVE_WALL_ORE_SILVER, Color.LIGHT_GRAY);
-        addMapping(Tile.TILE_CAVE_WALL_ORE_LEAD, Color.DARK_GRAY);
+        addMapping(Tile.TILE_CAVE_WALL_ORE_LEAD, Color.PINK.darker().darker());
         addMapping(Tile.TILE_CAVE_WALL_ORE_ZINC, new Color(235, 235, 235));
         addMapping(Tile.TILE_CAVE_WALL_SLATE, Color.BLACK);
         addMapping(Tile.TILE_CAVE_WALL_MARBLE, Color.WHITE);
